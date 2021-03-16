e7ad9d6c85ae653c92bd07d6fa1a636356bc6924
josineisilva <josinei.lopessilva@gmail.com>
2021-03-16 10:46:56 -0300

    removendo setters do DTO

--- src/main/java/br/unitau/inf/student/dto/StudentPostDTO.java
+++ src/main/java/br/unitau/inf/student/dto/StudentPostDTO.java
@@ -16,24 +16,12 @@ public class StudentPostDTO {
 		return name;
 	}
 
-	public void setName(String name) {
-		this.name = name;
-	}
-
 	public Integer getAge() {
 		return age;
 	}
 
-	public void setAge(Integer age) {
-		this.age = age;
-	}
-
 	public String getEmail() {
 		return email;
-	}
-
-	public void setEmail(String email) {
-		this.email = email;
 	}
 
 	public void update(Student obj) {
